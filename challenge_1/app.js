b88c91a6f3d665636997cd63866ef732c60501a6
Arun Bhardwaj <arun.b.95@gmail.com>
2021-10-20 16:36:29 -0400

    finished tic tac toe

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,32 +1,117 @@
+// CHANGE THESE VALUES FOR CHANGING BOARD SIZE
+const _ROWS = 3;
+const _COLS = 3;
+const _Player1 = prompt('Name Player 1:') || 'Player X';
+const _Player2 = prompt('Name Player 2:') || 'Player O';
 
+//TODO: Implement Draws and maybe have an in-memory Board like in n-queens
 
 function playSpace(e) {
-  (e.target.innerText === 'X')
-    ? e.target.innerText = 'O'
-    : e.target.innerText = 'X'
+  e.target.innerText = _turn
+  changeTurn();
+  checkForWinner();
 }
 
-function clearBoard(e) {
-  console.log('clearing');
+function changeTurn() {
+  (_turn === 'X')
+    ? _turn = 'O'
+    : _turn = 'X'
 }
 
-const addEventListenersToSpaces = () => {
-  //TODO: add event listeners
+function clearBoard() {
+  forEach(document.getElementsByClassName('space'), (element) => {
+    element.innerText = '';
+  })
+  _turn = 'X';
 }
 
-let x = document.getElementsByClassName('space')
-for (let i = 0; i < x.length; i++) {
-  let y = x.item(i);
-  y.addEventListener('click', playSpace);
+function checkForWinner() {
+  _checkRows();
+  _checkCols();
+  _checkDiagonals();
 }
 
-const forEach = (HTMLCollection, callback) => {
-  for (let i = 0; i < HTMLCollection.length; i++) {
-    let element = HTMLCollection.item(i);
-    callback.call(e);
+function _checkDiagonals() {
+  _checkMajorDiagonal();
+  _checkMinorDiagonal()
+}
+
+function _checkMajorDiagonal() {
+  let str = '';
+  let row = 1, col = 1;
+  while (row <= _ROWS && col <= _COLS) {
+    str += document.getElementById(`row-${row}`).getElementsByClassName(`col${col}`).item(0).innerText;
+    row++; col++;
+  }
+  _alertIfWon(str);
+}
+
+function _checkMinorDiagonal() {
+  let str = '';
+  let row = 1, col = _COLS;
+  while (row <= _ROWS && col >= 1) {
+    str += document.getElementById(`row-${row}`).getElementsByClassName(`col${col}`).item(0).innerText;
+    row++; col--;
+  }
+  _alertIfWon(str);
+}
+
+function _checkCols() {
+  for (let i = 1; i <= _COLS; i++) {
+    let str = '';
+    forEach(document.getElementsByClassName('col' + i), (element) => {
+      str += element.innerText;
+    })
+    _alertIfWon(str);
   }
 }
 
-// .forEach(element => element.addEventListener('click', playSpace));
+function _checkRows() {
+  forEach(document.getElementsByClassName('row'), (rows) => {
+    let str = '';
+    forEach(rows.cells, (element) => {
+      if (element.classList.contains('space')) {
+        str += element.innerText;
+      }
+    })
+    _alertIfWon(str);
+  })
+}
 
+function _alertIfWon(string) {
+  // TODO: Right now, the winning DOM element doesn't get placed before the alert happens.
+  // Console.logs will fix this, but figure a way around it.
+  // Tried ==> Using a callback in playSpace that would then invoke changeTurn and
+  // checkForWinner did NOT work.
+  if (string === 'XXX' ) {
+    alert(`${_Player1} wins!`);
+  } else if (string === 'OOO') {
+    alert(`${_Player2} wins!`);
+  }
+}
+
+function forEach(HTMLCollection, callback) {
+  for (let i = 0; i < HTMLCollection.length; i++) {
+    let element = HTMLCollection.item(i);
+    callback(element);
+  }
+}
+
+const addEventListenersToSpaces = () => {
+  forEach(document.getElementsByClassName('space'), (element) => {
+    element.addEventListener('click', playSpace);
+  })
+}
+
+//////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////
+
+let _turn = 'X';
+
+// Clear board
 document.getElementById('clear-btn').addEventListener('click', clearBoard)
+
+// Are callbacks always async?
+addEventListenersToSpaces();
+
